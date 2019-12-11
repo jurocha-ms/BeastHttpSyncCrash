dbad6842069a87d18cf5f7c5e06768f58656b438
Julio C. Rocha <julio@rochsquadron.net>
2019-12-10 18:16:59 -0800

    Finished New-MsvcProject.

--- Scripts/New-MsvcProject.ps1
+++ Scripts/New-MsvcProject.ps1
@@ -3,8 +3,16 @@ param(
     [string] $Name
 )
 
-[System.IO.Directory] $solutionDir = Split-Path -Path $PSScriptRoot
+[System.IO.DirectoryInfo] $solutionDir = Split-Path -Path $PSScriptRoot
 
-Copy-Item -Recurse "$solutionDir\Template" -Destination "$solutionDir\$Name"
+Copy-Item -Recurse "$solutionDir\Template" -Destination "$solutionDir\$Name" -Exclude *.user
 
-Get-ChildItem
+Get-ChildItem $solutionDir\$Name\Template.* | ForEach-Object {
+    Move-Item $_ ("$_" -replace 'Template', $Name)
+}
+
+[xml] $xml = Get-Content $solutionDir\$Name\$Name.vcxproj
+($xml.Project.PropertyGroup | Where-Object 'Label' -eq 'Globals').ProjectGuid = '{' + [guid]::NewGuid().ToString().ToUpper() + '}'
+$xml.Save((Get-ChildItem $solutionDir\$Name\$Name.vcxproj))
+
+Write-Host "Created $Name\$Name.vcxproj"
